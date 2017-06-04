1ef361bbee4ec1406a06a99adc4275e0e0d16217
Sriram Patil <spsrirampatil@gmail.com>
2017-06-04 19:58:07 +0530

    Added comment in forward_list.go

--- forward_list/forward_list.go
+++ forward_list/forward_list.go
@@ -16,6 +16,7 @@ type ForwardList struct {
 	size     int
 }
 
+// NewForwardList creates a new ForwardList and returns a pointer to the same.
 func NewForwardList() *ForwardList {
 	list := new(ForwardList)
 	list.size = 0
@@ -27,15 +28,18 @@ func NewForwardList() *ForwardList {
 	return list
 }
 
+// Next moves the Iterator to the next node.
 func (itr *Iterator) Next() *Iterator {
 	itr.node = itr.node.next
 	return itr
 }
 
+// Value returns the value stored in the node pointed by the Iterator.
 func (itr *Iterator) Value() interface{} {
 	return itr.node.value
 }
 
+// Equals returns true if both Iterators are pointing to the same node.
 func (lhs *Iterator) Equals(rhs *Iterator) bool {
 	if lhs.list != rhs.list {
 		return false
@@ -47,14 +51,18 @@ func (lhs *Iterator) Equals(rhs *Iterator) bool {
 	return false
 }
 
+// Empty return true if the ForwardList is empty.
 func (list *ForwardList) Empty() bool {
 	return (list.size == 0)
 }
 
+// Size returns the number of nodes in the ForwardList.
 func (list *ForwardList) Size() int {
 	return list.size
 }
 
+// Front returns the values stores in the first node of the ForwardList if it is
+// not empty.
 func (list *ForwardList) Front() interface{} {
 	if !list.Empty() {
 		return list.sentinel.next.value
@@ -62,6 +70,8 @@ func (list *ForwardList) Front() interface{} {
 	return nil
 }
 
+// PushFront creates a new node containing value v and adds it to the start of
+// the ForwardList.
 func (list *ForwardList) PushFront(v interface{}) {
 	node := new(listNode)
 	node.value = v
@@ -70,6 +80,7 @@ func (list *ForwardList) PushFront(v interface{}) {
 	list.size++
 }
 
+// PopFront removed the first node from the ForwardList.
 func (list *ForwardList) PopFront() {
 	if !list.Empty() {
 		node := list.sentinel.next
@@ -79,11 +90,13 @@ func (list *ForwardList) PopFront() {
 	}
 }
 
+// Clear removes all the nodes from the ForwardList.
 func (list *ForwardList) Clear() {
 	list.sentinel.next = list.sentinel
 	list.size = 0
 }
 
+// Begin returns an Iterator pointing to the first node of the ForwardList.
 func (list *ForwardList) Begin() *Iterator {
 	itr := new(Iterator)
 	itr.node = list.sentinel.next
@@ -91,10 +104,12 @@ func (list *ForwardList) Begin() *Iterator {
 	return itr
 }
 
+// End returns an Iterator pointing to the node past-the-end of the ForwardList.
 func (list *ForwardList) End() *Iterator {
 	return list.endItr
 }
 
+// Reverse reverses the ForwardList in place.
 func (list *ForwardList) Reverse() {
 	var prev, curr, next *listNode = list.sentinel, list.sentinel.next, list.sentinel.next.next
 
